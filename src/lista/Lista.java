26e02a7cfd0feb4991dc702f6b2798cc1527a50c
Tiago-Mendoza <tiagomzdev@gmail.com>
2026-03-24 22:05:39 -0300

    Atualizando

--- src/lista/Lista.java
+++ src/lista/Lista.java
@@ -3,17 +3,62 @@ package lista;
 import modelo.Produto;
 
 public class Lista {
-    private No inicio, aux, atual;
 
+    private No inicio, auxiliar, atual;
+
+    /**
+     * Insere nó no final da lista simplesmente ligada
+     * @param produto Produto
+     */
     public void inserir(Produto produto){
-        //Quando lista estiver vazia
-        if(inicio==null){
-            inicio=new No(produto,null);
-            aux=inicio;
-        }else {//quando a lista não estiver vazia
-            atual = new No(produto,null);
-            atual.setProx(atual);
-            aux=atual;
+        // Quando a lista estiver vazia
+        if(inicio == null){
+            inicio = new No(produto, null);
+            auxiliar = inicio;
         }
+        else {
+            // Quando a lista não estiver vazia
+            atual = new No(produto, null);
+            auxiliar.setProx(atual);
+            auxiliar = atual;
+        }
+    }
+
+    /**
+     * Exibe todos os produtos da lista
+     */
+    public void exibir(){
+        No e = inicio;
+        while(e != null){
+            System.out.println(e.getProduto());
+            e = e.getProx();
+        }
+    }
+
+
+    // NOVAS MODIFICAÇÕES ABAIXO
+
+
+    public Produto pesquisar(int id) {
+        No e = inicio;
+        while (e != null) {
+            if (e.getProduto().getId() == id) {
+                return e.getProduto();
+            }
+            e = e.getProx();
+        }
+        return null;
+    }
+
+    // NOVAS MODIFICAÇÕES ABAIXO
+
+    public boolean atualizar(int id, float preco) {
+        Produto p = pesquisar(id);
+
+        if (p != null) {
+            p.setPreco(preco);
+            return true;
+        }
+        return false;
     }
 }
